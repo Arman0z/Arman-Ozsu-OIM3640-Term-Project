87320f73cd803c2513149cc46651147c11a6d807
Arman0z <143458194+Arman0z@users.noreply.github.com>
2023-12-08 17:56:52 -0500

    update

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
+print("Loading Libraries")
 from nba_api.stats.endpoints import playercareerstats
+
+from nba_api.live.nba.endpoints import scoreboard
+from nba_api.stats.static import teams
+from nba_api.stats.static import players   
+import numpy
+import pandas
 import pprint
 
-# Nikola Jokić
-career = playercareerstats.PlayerCareerStats(player_id='203999') 
+print ("\n\n\nBeg.\n\n\n")
 
-# pandas data frames (optional: pip install pandas)
-career.get_data_frames()[0]
+# Nikola Jokić
+nikolaJ = playercareerstats.PlayerCareerStats(player_id='203999') 
+
+# Today's Score Board
+games = scoreboard.ScoreBoard()
 
 # json
-career.get_json()
+games.get_json()
 
 # dictionary
-career.get_dict()
+games.get_dict()
 
-print(career.get_dict())
+ad = playercareerstats.PlayerCareerStats(player_id="203076")
+print(ad.get_data_frames()[0])
+
+# get_teams returns a list of 30 dictionaries, each an NBA team.
+nba_teams = teams.get_teams()
+print("Number of teams fetched: {}".format(len(nba_teams)))
+nba_teams[:3]
+print(nba_teams)
+
+nba_players = players.get_players()
+print("Number of players fetched: {}".format(len(nba_players)))
+nba_players[:5]
+# print(nba_players)
+
+spurs = [team for team in nba_teams if team["full_name"] == "San Antonio Spurs"][0]
+print(spurs)
+
+big_fundamental = [
+    player for player in nba_players if player["full_name"] == "Tim Duncan"
+][0]
+print(big_fundamental)
+
+print ("\n\n\nEnd.")
